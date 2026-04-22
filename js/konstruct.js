26ae8afbe6b7fdda47cb25da9e581ffb4715b4dd
Lenin <275s1137@list.ru>
2026-04-22 19:40:18 +0300

    final

--- js/konstruct.js
+++ js/konstruct.js
@@ -1,7 +1,12 @@
-const btn = document.querySelector(".header__menu-btn")
-const menu = document.querySelector(".menu")
-const addClass = document.querySelector(".header-main-bottom")
+// const btn = document.querySelector(".header__menu-btn")
+// const menu = document.querySelector(".menu")
+// const addClass = document.querySelector(".header-main-bottom")
 
-btn.addEventListener("click", () => {
-    addClass.classList.toggle("open")
+// btn.addEventListener("click", () => {
+//     addClass.classList.toggle("open")
+// })
+
+document.addEventListener('DOMContentLoaded', function(){
+    let navEl = document.querySelectorAll('.sidenav');
+    let navInst = M.Sidenav.init(navEl)
 })
